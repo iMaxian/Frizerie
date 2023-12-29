add829fd68d31289d62425e1f85664ff2edb0f48
IONEL MAXIAN <ionel.maxian@stud.ubbcluj.ro>
2023-12-29 22:16:59 +0200

    master1

--- Models/Barber.cs
+++ Models/Barber.cs
@@ -26,14 +26,6 @@ namespace Frizerie.Models
         [Range(1, 5, ErrorMessage = "Ratingul trebuie să fie între 1 și 5.")]
         public int Rateing { get; set; }
         public ICollection<Serviciu>? Servicii { get; set; }
-        
-        /*
-        public int Id { get; set; }
-        public string Nume { get; set; }
-        public string Prenume { get; set; }
-        public string NrTelefon { get; set; }
-        public string Email { get; set; }
-        public int Rateing { get; set; }
-        */
+
     }
 }
--- Models/Serviciu.cs
+++ Models/Serviciu.cs
@@ -13,16 +13,15 @@ namespace Frizerie.Models
         public string Descriere { get; set; }
         public decimal Cost { get; set; }
         */
-        [Display(Name = "Serviciu")]
-        [RegularExpression(@"^[A-Z]+[a-z\s]*$", ErrorMessage = "Denumirea serviciului trebuie să înceapă cu majusculă și să aibă o lungime minimă de caractere 3")]
-        [StringLength(70, MinimumLength = 3)]
+        [Display(Name = "Tip Serviciu")]
+        [RegularExpression(@"^[A-Z]+[a-z\s]*$", ErrorMessage = "Trebuie să înceapă cu majusculă și să exprime serviciul dorit")]
         public string Tip_Serviciu { get; set; }
 
         public int? BarberID { get; set; }
         public Barber? Barber { get; set; }
 
         [Column(TypeName = "decimal(6, 2)")]
-        [Range(0.01, 14000)]
+        [Range(10, 1000, ErrorMessage = "Introduceți o valoare între 10 și 1000 lei")]
         [Display(Name = "Cost")]
         public decimal Cost { get; set; }
 
